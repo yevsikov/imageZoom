3a216514cfc734678c5f80a40e74071edb4c0d90
Vladislav Yevsikov <v.yevsikov@gmail.com>
2019-01-21 13:02:22 +0200

    Add Comments

--- src/imageZoom.js
+++ src/imageZoom.js
@@ -5,23 +5,24 @@ Link: https://github.com/yevsikov/imageZoom
 */
 (function ( $ ) {
     $( document ).ready(function () {
-
+        //if zoomedArea element is missing -  create new
         if ( $( '#zoomedArea' ).length<1 ) {
             $( '<div id="zoomedArea" style="z-index:999;"></div>' ).appendTo( 'body' );
         }
-        var zoomedElement = $( '.zoom-js' );
+        var zoomedElement = $( 'img.zoom-js' );
         var zoomedArea = $( '#zoomedArea' );
+        //set margin for zoomedArea
         var margin = 20;
-
+        //if images for zooming found - make some action for its hover
         if ( zoomedElement.length > 0 ) {
             zoomedElement.hover(function() {
                 var currentThumb = $(this);
-
+                //get coordinates of current image for zoomedArea hiding
                 zoomedArea.attr( 'data-outx', ( currentThumb.offset().left+currentThumb.width() ) );
                 zoomedArea.attr( 'data-outy', ( currentThumb.offset().top+currentThumb.height() ) );
                 zoomedArea.attr( 'data-inx', ( currentThumb.offset().left ) ) ;
                 zoomedArea.attr( 'data-iny', ( currentThumb.offset().top ) ) ;
-
+                //if fullsize image doesn't set - use the same image, but with some css zoom
                 if ( currentThumb.attr('data-fullsize') ) {
                     var fullsizeImage = currentThumb.attr( 'data-fullsize' );
                     zoomedArea.css( 'background-size', 'unset' );
@@ -34,26 +35,25 @@ Link: https://github.com/yevsikov/imageZoom
                         zoomedArea.css( 'background-size', 'unset' );
                     }
                 }
-
+                //get callback data if exists
                 zoomedArea.attr( 'data-callback', ( currentThumb.attr('data-callback') ? currentThumb.attr('data-callback'): '') ) ;
-
+                //change styles for zoomedArea (bg, coords, position)
                 zoomedArea.css( 'background-image', 'url(' + fullsizeImage + ')' );
-
                 zoomedArea.css( 'top',  currentThumb.offset().top-margin );
                 zoomedArea.css( 'left', currentThumb.offset().left-margin  );
                 zoomedArea.css( 'height',  currentThumb.height()+margin*2 );
                 zoomedArea.css( 'width', currentThumb.width()+margin*2  );
-
                 zoomedArea.css( 'position', 'absolute'  );
-
+                //show zoomedArea
                 zoomedArea.show();
             });
         } else {
             console.error( 'ERROR: .zoom-js elements are not found' );
         }
-
+        //listen for mouse moving out of zoomed image area
         if ( zoomedArea.length > 0 ) {
             $( 'body' ).mousemove(function( event ) {
+                //check if cursor coordinates are out of zoomed image area
                 if (
                     ( (event.pageY-margin)>zoomedArea.attr( 'data-outy' ) )
                     ||
@@ -63,19 +63,21 @@ Link: https://github.com/yevsikov/imageZoom
                     ||
                     ( (event.pageX+margin)<zoomedArea.attr( 'data-inx' ) )
                 ) {
+                    //hide zoomedArea and clear callback
                     zoomedArea.hide();
                     zoomedArea.attr( 'data-callback', '' ) ;
                 } else {
+                    //scroll zoomed image with mouse moving (X coords)
                     var fullX = zoomedArea.attr('data-outx')-zoomedArea.attr('data-inx')+margin*2;
                     var positionX =  (( event.pageX - zoomedArea.attr('data-inx')+margin )*100)/fullX;
                     zoomedArea.css( 'background-position-x', positionX+'%' );
-
+                    //scroll zoomed image with mouse moving (Y coords)
                     var fullY = zoomedArea.attr('data-outy')-zoomedArea.attr('data-iny')+margin*2;
                     var positionY =  (( event.pageY - zoomedArea.attr('data-iny')+margin )*100)/fullY;
                     zoomedArea.css( 'background-position-y', positionY+'%' );
                 }
             });
-
+            //hide zoomedArea and clear callback after window resize
             $( window ).resize(function() {
                 zoomedArea.hide();
                 zoomedArea.attr( 'data-callback', '' ) ;
@@ -83,6 +85,5 @@ Link: https://github.com/yevsikov/imageZoom
         } else {
             console.error( 'ERROR: #zoomedArea element is not found' );
         }
-
     });
 })(jQuery);
